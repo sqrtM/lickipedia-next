a18365aa08c32ce3e133fe52c63d7273032cebb1
mason <mpike21797@gmail.com>
2022-10-29 14:51:11 +0200

    update todo

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,8 +3,10 @@ import Feed from '../src/Feed';
 
 /*
 Problems to fix:
-1.) because I am reversing the map array, the unique IDs get placed backwards from the HTML elements.
-Find a way to ensure that the feed goes backwards, but the IDs remain the same (unshifting ???).
+1.) a like button AND a save button is probably unnecessary since this isn't a social media webside.
+we will get rid of like, and change "edit" to "fork" so it will work much the same way 
+shadersandbox or Hydrasandbox work, wherein you can fork someone's lick and create your own.
+ideally with a lineage shown as well. Easy transposition is also a necessity.
 
 2.) Add additional information to the feed items. Date, author???, and tags are a minimum, but 
 maybe there's other stuff worth putting there. 
