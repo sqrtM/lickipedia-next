63723bf55b3809ce6a25cf642a0b3cd1d8f16c08
mason <mpike21797@gmail.com>
2022-10-30 22:27:14 +0100

    why deploy is so hard

--- src/next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/sqrtM/lickipedia-next.github.io/gh-pages/' : '',
-}
